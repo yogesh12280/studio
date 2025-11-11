57062737d2d9846eafc80faa6281d088c11b8213
yogesh patel <yogesh12280@gmail.com>
2025-11-11 17:22:17 +0000

    "Appreciation" page layout should be like "Notifications" page.

--- src/app/appreciation/page.tsx
+++ src/app/appreciation/page.tsx
@@ -73,7 +73,7 @@ export default function AppreciationPage() {
           onAddAppreciation={handleAddAppreciation}
         />
         <main className="p-4 sm:p-6">
-            <div className="space-y-4 max-w-4xl mx-auto">
+            <div className="space-y-4">
                 {filteredAppreciations.map(appreciation => (
                     <AppreciationCard 
                         key={appreciation.id}
