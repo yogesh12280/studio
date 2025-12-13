5b9b4149542a3f7dfce4ef605dc41906f901f4f5
yogesh patel <yogesh12280@gmail.com>
2025-12-13 08:47:09 +0000

    modify the calender for schedule

--- src/components/create-notification-dialog.tsx
+++ src/components/create-notification-dialog.tsx
@@ -257,7 +257,7 @@ export function CreateNotificationDialog(props: NotificationDialogProps) {
                           {scheduledFor ? format(scheduledFor, 'PPP') : <span>Pick a post date</span>}
                         </Button>
                       </PopoverTrigger>
-                      <PopoverContent onPointerDownOutside={(e) => e.preventDefault()} className="w-auto p-0">
+                      <PopoverContent className="w-auto p-0" onPointerDownOutside={(e) => e.preventDefault()}>
                         <Calendar
                           mode="single"
                           selected={scheduledFor}
@@ -285,7 +285,7 @@ export function CreateNotificationDialog(props: NotificationDialogProps) {
                           {endDate ? format(endDate, 'PPP') : <span>Pick an expiry date</span>}
                         </Button>
                       </PopoverTrigger>
-                      <PopoverContent onPointerDownOutside={(e) => e.preventDefault()} className="w-auto p-0">
+                      <PopoverContent className="w-auto p-0" onPointerDownOutside={(e) => e.preventDefault()}>
                         <Calendar
                           mode="single"
                           selected={endDate}
