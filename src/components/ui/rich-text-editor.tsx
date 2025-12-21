28e167faaddb5bebdc8a49b1a0b3903b257a09c9
yogesh patel <yogesh12280@gmail.com>
2025-12-21 16:29:15 +0000

    text editor is not working. please do required changes in exisiting file

--- src/components/ui/rich-text-editor.tsx
+++ src/components/ui/rich-text-editor.tsx
@@ -1,7 +1,106 @@
-import dynamic from "next/dynamic";
+"use client";
 
-const Editor = dynamic(() => import("./CKEditorClient"), {
-  ssr: false,
-});
+import { useEffect, useRef, useState } from "react";
+import { Skeleton } from "./skeleton";
 
-export { Editor as RichTextEditor };
+interface RichTextEditorProps {
+  value: string;
+  onChange: (data: string) => void;
+}
+
+export function RichTextEditor({ value, onChange }: RichTextEditorProps) {
+  const editorRef = useRef<any>(null);
+  const [isClient, setIsClient] = useState(false);
+  const { CKEditor, CustomEditor } = editorRef.current || {};
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  useEffect(() => {
+    if (isClient) {
+      import("@ckeditor/ckeditor5-react").then((module) => {
+        editorRef.current = {
+          ...editorRef.current,
+          CKEditor: module.CKEditor,
+        };
+      });
+      import("@/lib/ckeditor/custom-editor").then((module) => {
+        editorRef.current = {
+          ...editorRef.current,
+          CustomEditor: module.default,
+        };
+      });
+    }
+  }, [isClient]);
+
+  if (!isClient || !CKEditor || !CustomEditor) {
+    return <Skeleton className="h-48 w-full" />;
+  }
+
+  return (
+    <CKEditor
+      editor={CustomEditor}
+      data={value}
+      config={{
+        toolbar: [
+          "heading",
+          "|",
+          "bold",
+          "italic",
+          "underline",
+          "strikethrough",
+          "|",
+          "fontFamily",
+          "fontSize",
+          "fontColor",
+          "fontBackgroundColor",
+          "|",
+          "alignment",
+          "|",
+          "bulletedList",
+          "numberedList",
+          "|",
+          "link",
+          "insertTable",
+          "|",
+          "imageUpload",
+          "|",
+          "undo",
+          "redo",
+        ],
+        image: {
+          toolbar: [
+            "imageTextAlternative",
+            "imageStyle:inline",
+            "imageStyle:block",
+            "imageStyle:side",
+            "resizeImage",
+          ],
+          resizeOptions: [
+            { name: "resizeImage:original", value: null },
+            { name: "resizeImage:50", value: "50" },
+            { name: "resizeImage:75", value: "75" },
+          ],
+        },
+        fontFamily: {
+          options: [
+            "default",
+            "Arial, Helvetica, sans-serif",
+            "Courier New, Courier, monospace",
+            "Georgia, serif",
+            "Times New Roman, Times, serif",
+            "Verdana, Geneva, sans-serif",
+          ],
+        },
+        fontSize: {
+          options: [10, 12, 14, "default", 18, 20, 24, 32],
+        },
+      }}
+      onChange={(event: any, editor: any) => {
+        const data = editor.getData();
+        onChange(data);
+      }}
+    />
+  );
+}
